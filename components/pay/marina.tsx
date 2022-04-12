517fb92f0a58fd566f52b1979c21973d25679cb6
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-04-12 17:28:30 +0000

    adds latin

--- components/pay/marina.tsx
+++ components/pay/marina.tsx
@@ -32,34 +32,43 @@ const Marina = ({ contract, topup, setResult }: MarinaProps) => {
   const { ticker, value } = contract.collateral
   const quantity = topup || contract.collateral.quantity
   return (
-    <div className="columns">
-      <div className="column is-6 my-auto">
-        <button
-          className="button is-primary"
-          onClick={() => openModal('marina-modal')}
-        >
-          <Image
-            src="/images/marina.svg"
-            alt="marina logo"
-            width={20}
-            height={20}
-          />
-          <span className="ml-2">Deposit with Marina</span>
-        </button>
-      </div>
-      <div className="column is-6 has-pink-border info-card px-5 py-4">
-        <p className="amount">Amount to deposit</p>
-        <div className="is-flex is-justify-content-space-between">
-          <p className="quantity my-auto">
-            {prettyNumber(quantity)} {ticker}
-          </p>
-          <p className="value my-auto">
-            US ${prettyNumber((quantity || 0) * value)}
-          </p>
+    <>
+      <div className="columns">
+        <div className="column is-6 my-auto">
+          <button
+            className="button is-primary"
+            onClick={() => openModal('marina-modal')}
+          >
+            <Image
+              src="/images/marina.svg"
+              alt="marina logo"
+              width={20}
+              height={20}
+            />
+            <span className="ml-2">Deposit with Marina</span>
+          </button>
+        </div>
+        <div className="column is-6 has-pink-border info-card px-5 py-4">
+          <p className="amount">Amount to deposit</p>
+          <div className="is-flex is-justify-content-space-between">
+            <p className="quantity my-auto">
+              {prettyNumber(quantity)} {ticker}
+            </p>
+            <p className="value my-auto">
+              US ${prettyNumber((quantity || 0) * value)}
+            </p>
+          </div>
         </div>
       </div>
+      <p className="is-size-7 mt-6">
+        In vivamus mi pretium pharetra cursus lacus, elit. Adipiscing eget vel
+        ut non duis vitae. Augue mi, bibendum ac imperdiet ipsum sed ornare.
+        Facilisis id sem quam elementum euismod ante ut. Ac, pharetra elit, sit
+        pharetra a. Eu diam nunc nulla risus arcu, integer nulla diam, est. Nisl
+        accumsan potenti mattis consectetur pellentesque.
+      </p>
       <MarinaModal contract={contract} setResult={setResult} topup={topup} />
-    </div>
+    </>
   )
 }
 
