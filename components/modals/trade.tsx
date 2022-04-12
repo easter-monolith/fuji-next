3a0fe5e9d3c2a584405518be5062370bd3ee5f2c
easter-monolith <101349954+easter-monolith@users.noreply.github.com>
2022-04-12 17:55:50 +0000

    new trade modal

--- components/modals/trade.tsx
+++ components/modals/trade.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Modal from './modal'
 
 const TradeModal = () => {
@@ -6,18 +7,62 @@ const TradeModal = () => {
       <h3>Trade</h3>
       <p>Trade your assets in this exchanges:</p>
       <div className="links-wrapper">
-        <p>
-          <a href="https://play.google.com/store/apps/details?id=io.sevenlabs.app">
-            TDEX Mobile App
+        <div className="columns my-0">
+          <div className="column is-6 py-0">
+            <p>
+              <a href="#">
+                TDEX iOS App
+                <span className="icon-container">
+                  <Image
+                    src="/images/apple.svg"
+                    alt="apple logo"
+                    height={20}
+                    width={20}
+                  />
+                </span>
+              </a>
+            </p>
+          </div>
+          <div className="column is-6 py-0">
+            <p>
+              <a href="https://play.google.com/store/apps/details?id=io.sevenlabs.app">
+                TDEX Android App
+                <span className="icon-container">
+                  <Image
+                    src="/images/google.svg"
+                    alt="google logo"
+                    height={20}
+                    width={20}
+                  />
+                </span>
+              </a>
+            </p>
+          </div>
+        </div>
+        <p className="mt-0">
+          <a href="https://beta.bitmatrix.app/">
+            Bitmatrix
           </a>
         </p>
         <p>
-          <a href="https://beta.bitmatrix.app/">Bitmatrix</a>
-        </p>
-        <p>
-          <a href="http://dex.vulpem.com">dex.vulpem.com</a>
+          <a href="http://dex.vulpem.com">
+            dex.vulpem.com
+          </a>
         </p>
       </div>
+      <style jsx>{`
+        div.links-wrapper p {
+          max-width: 100%;
+        }
+        div.links-wrapper p a {
+          font-size: 1rem;
+        }
+        span.icon-container {
+          position: relative;
+          left: 10px;
+          top: 4px;
+        }
+      `}</style>
     </Modal>
   )
 }
